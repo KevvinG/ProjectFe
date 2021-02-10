9d4dbc5ed6a22322c040fc118e14417b821e1923
Jayce <merinchu@sheridancollege.ca>
2021-02-10 15:47:35 -0500

    set up GoogleServices File and imported Firebase as per tutorial

---
+++ Fe/Pods/Target Support Files/Pods-Fe-FeUITests/Pods-Fe-FeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fe_FeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Fe_FeUITests
+@end
---
+++ Fe/Pods/Target Support Files/Pods-FeTests/Pods-FeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FeTests : NSObject
+@end
+@implementation PodsDummy_Pods_FeTests
+@end
