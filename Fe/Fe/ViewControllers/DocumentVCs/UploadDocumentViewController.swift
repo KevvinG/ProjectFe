96e37a7d46852a6f3c86896275130ab704cf3553
Jayce <merinchu@sheridancollege.ca>
2021-03-14 17:41:31 -0400

    Successfully upload image to Firebase Storage using Gallery or Camera. Just need to adjust the Date format for folders.

--- Fe/Fe/ViewControllers/DocumentVCs/UploadDocumentViewController.swift
+++ Fe/Fe/ViewControllers/DocumentVCs/UploadDocumentViewController.swift
@@ -24,7 +24,8 @@ class UploadDocumentViewController: UIViewController {
     @IBOutlet var txtDoctorName: UITextField!
     @IBOutlet var txtTestResults: UITextField!
     @IBOutlet var Notes: UITextField!
-    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var imagePicked: UIImageView!
+    
     
     
     /*--------------------------------------------------------------------
@@ -46,7 +47,6 @@ class UploadDocumentViewController: UIViewController {
         let dateFormatter = DateFormatter()
         dateFormatter.dateStyle = DateFormatter.Style.medium
         dateFormatter.timeStyle = DateFormatter.Style.medium
-        let strDate = dateFormatter.string(from: docDatePicker.date)
         self.view.endEditing(true)
     }
     
@@ -67,39 +67,73 @@ class UploadDocumentViewController: UIViewController {
      - Description: gets the string of the datePicker
      -------------------------------------------------------------------*/
     @IBAction func takePictureBtnTapped() {
-        let picker = UIImagePickerController()
-        picker.sourceType = .camera
-        picker.allowsEditing = true
-        picker.delegate = self
-        present(picker, animated: true)
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.allowsEditing = true
+            picker.delegate = self
+            present(picker, animated: true)
+        } else {
+            print("Camera not available.")
+        }
+    }
+    
+    /*--------------------------------------------------------------------
+     - Function: findFileBtnTapped()
+     - Description: Opens photo library to pick image
+     -------------------------------------------------------------------*/
+    @IBAction func findFileBtnTapped(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let image = UIImagePickerController()
+            image.delegate = self
+            image.sourceType = .photoLibrary
+            image.allowsEditing = true
+            self.present(image, animated: true, completion: nil)
+        } else {
+            print("Photo Library not available.")
+        }
     }
     
     /*--------------------------------------------------------------------
      - Function: uploadBtnTapped()
      - Description: 
      -------------------------------------------------------------------*/
-    func uploadBtnTapped() {
-        // Get the user email and check if they are already in the storage system
-        let user = Auth.auth().currentUser
-        let usersRef = db.collection("users")
-        usersRef.whereField("email", isEqualTo: user?.email ?? "NOEMAIL")
-        
-        // Get DatePicker date
-        let date = getDatePickerString()
-        
-        let uploadRef = Storage.storage().reference(withPath: "\(String(describing: user?.email))/\(date)\(String(describing: txtTestName.text)).jpg")
-        
-        guard let imageData = imgView.image?.jpegData(compressionQuality: 0.75) else { return }
-        
-        let uploadMetadata = StorageMetadata.init()
-        uploadMetadata.contentType = "image/jpeg"
-        
-        uploadRef.putData(imageData, metadata: uploadMetadata) { (downloadMetadata, error) in
-            if let error = error {
-                print("Error uploading picture: \(error.localizedDescription)")
-                return
+    @IBAction func uploadBtnTapped(_ sender: Any) {
+        if imagePicked.image == nil {
+            // Show Alert to pick an image first
+            let errorMessage = UIAlertController(title: "Error!", message: "Please take a picture or select one from the gallery first!", preferredStyle: .alert)
+            let confirm = UIAlertAction(title: "OK", style: .default, handler: nil)
+            errorMessage.addAction(confirm)
+            self.present(errorMessage, animated: true, completion: nil)
+        } else {
+            // Get the user email and check if they are already in the storage system
+            let user = Auth.auth().currentUser
+            let usersRef = db.collection("users")
+            usersRef.whereField("email", isEqualTo: user?.email ?? "NOEMAIL")
+            
+            // Get DatePicker date
+            let date = getDatePickerString()
+            
+            let uploadRef = Storage.storage().reference(withPath: "\(String(describing: user?.email))/\(date)/\(String(describing: txtTestName.text)).jpg")
+            
+            guard let imageData = imagePicked.image?.jpegData(compressionQuality: 0.75) else { return }
+            
+            let uploadMetadata = StorageMetadata.init()
+            uploadMetadata.contentType = "image/jpeg"
+            
+            uploadRef.putData(imageData, metadata: uploadMetadata) { (downloadMetadata, error) in
+                if let error = error {
+                    print("Error uploading picture: \(error.localizedDescription)")
+                    return
+                }
+                print("Put is complete and i got this back: \(String(describing: downloadMetadata))")
             }
-            print("Put is complete and i got this back: \(String(describing: downloadMetadata))")
+            
+            // Show Alert that image was saved
+            let confirmationMessage = UIAlertController(title: "Picture Saved!", message: "Your picture has beed saved", preferredStyle: .alert)
+            let confirm = UIAlertAction(title: "OK", style: .default, handler: nil)
+            confirmationMessage.addAction(confirm)
+            self.present(confirmationMessage, animated: true, completion: nil)
         }
     }
 }
@@ -114,11 +148,13 @@ extension UploadDocumentViewController: UIImagePickerControllerDelegate, UINavig
         picker.dismiss(animated: true, completion: nil)
     }
     
+    /*--------------------------------------------------------------------
+     - Function: imagePickerController()
+     - Description: Handles when a picture is chosen on camera or photo library.
+     -------------------------------------------------------------------*/
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-    
-        picker.dismiss(animated: true, completion: nil)
-        
-        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else { return }
-        imgView.image = image
+        let image = info[UIImagePickerController.InfoKey(rawValue: UIImagePickerController.InfoKey.originalImage.rawValue)] as! UIImage
+        imagePicked.image = image
+        dismiss(animated: true, completion: nil)
     }
 }
