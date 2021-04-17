6dc23d6536a4f9fb3f8224c3760bcea5bb9b658e
Jayce <merinchu@sheridancollege.ca>
2021-04-16 21:35:50 -0400

    Fixed Login Screen Layout. Added scrolling to the Update Document Screen.

--- Fe/Fe/Business Layer/QuestionnaireLogic/Questionnaire.swift
+++ Fe/Fe/Business Layer/QuestionnaireLogic/Questionnaire.swift
@@ -5,10 +5,19 @@
 //  Created by Jayce Merinchuk on 2021-04-16.
 //
 
+// Imports
 import Foundation
 
+/*------------------------------------------------------------------------
+ - Class: Questionnaire
+ - Description:
+ -----------------------------------------------------------------------*/
 class Questionnaire {
     
+    /*--------------------------------------------------------------------
+     - Function: callDoctor()
+     - Description: fetch doctor phone number and open menu to call.
+     -------------------------------------------------------------------*/
     func callDoctor(completion: @escaping (_ doctorPhone: String) -> Void) {
         FirebaseAccessObject().fetchDoctorPhoneNumber( completion: { (phone) -> Void in
             completion(phone)
