fedea82965f2987ab3751e27efa5bff0620cc615
Jayce <merinchu@sheridancollege.ca>
2021-07-23 15:32:29 -0400

    Minor updates to Watch App UI. Updated the watch Code.

--- Fe/Fe-Mini Extension/InterfaceController.swift
+++ Fe/Fe-Mini Extension/InterfaceController.swift
@@ -5,43 +5,34 @@
 //  Created by Kevin Grzela on 2021-02-24.
 //
 
+//MARK: Imports
 import WatchKit
 import Foundation
 import HealthKit
 import WatchConnectivity
 
+/*------------------------------------------------------------------------
+ //MARK: InterfaceControlleer : WKInterfaceController, HKWorkoutSessionDelegate,
+ HKLiveWorkoutBuilderDelegate, WCSessionDelegate
+ - Description: Holds logic for interface of Apple Watch
+ -----------------------------------------------------------------------*/
 class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate, HKLiveWorkoutBuilderDelegate, WCSessionDelegate {
     
-   
+    // UI Variables
     @IBOutlet var startStopButton: WKInterfaceButton!
-    @IBOutlet var deviceLabel: WKInterfaceLabel!
     @IBOutlet var bpmLabel: WKInterfaceLabel!
+    @IBOutlet var bldOxLabel: WKInterfaceLabel!
     
-    // Our workout session
+    // Class Variables
     var session: HKWorkoutSession!
-    // Live workout builder
     var builder: HKLiveWorkoutBuilder!
-    // Tracking our workout state
     var workingOut = false
-    // Access point for all data managed by HealthKit.
     let healthStore = HKHealthStore()
-    var timerRunning : Bool = true
     
-    @IBAction func startStopTapped() {
-        if !workingOut {
-            startWorkout()
-            workingOut = true
-            startStopButton!.setTitle("Stop")
-            deviceLabel!.setText(builder!.device!.name)
-        } else {
-            stopWorkout()
-            workingOut = false
-            bpmLabel!.setText("---")
-            startStopButton!.setTitle("Start")
-            deviceLabel!.setText("Device Info")
-        }
-    }
-    
+    /*--------------------------------------------------------------------
+     //MARK: didAppear()
+     - Description: Initialize variables when screen loads.
+     -------------------------------------------------------------------*/
     override func didAppear() {
         // The quantity type to write to the health store.
         let typesToShare: Set = [
@@ -66,91 +57,43 @@ class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate, HKLi
             session.delegate = self
             session.activate()
         }
-        
-      //  self.timerRunning = true
-      //  customTimer()
-        
-        //_ = Timer.scheduledTimer(timeInterval: 30.0, target: self, selector: #selector(fire), userInfo: nil, repeats: true)
     }
     
-    //@objc func fire()
-    func fire()
-    {
-        print("Fired")
+    /*--------------------------------------------------------------------
+     //MARK: startStopTapped()
+     - Description: Tap the Start/Stop button on screen.
+     -------------------------------------------------------------------*/
+    @IBAction func startStopTapped() {
         if !workingOut {
-            print("Start WO")
             startWorkout()
             workingOut = true
             startStopButton!.setTitle("Stop")
-            deviceLabel!.setText(builder!.device!.name)
         } else {
-            print("Stop WO")
             stopWorkout()
             workingOut = false
-            bpmLabel!.setText("---")
             startStopButton!.setTitle("Start")
-            deviceLabel!.setText("Device Info")
         }
     }
     
-    private func customTimer(){
-        if (self.timerRunning){
-                DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + .milliseconds(9000)) {
-                    self.customTimer()
-                    print("Timer loop")
-                }
-
-                fire()
-
-            }
-
-        }
-    
-    func initWorkout() {
-        let configuration = HKWorkoutConfiguration()
-        configuration.activityType = .crossTraining
-        
-        configuration.locationType = .indoor
-        
-        do {
-            session = try HKWorkoutSession(healthStore: healthStore, configuration: configuration)
-            builder = session.associatedWorkoutBuilder()
-        } catch {
-            fatalError("Unable to create the workout session!")
-        }
-        
-        // Setup session and builder.
-        session.delegate = self
-        builder.delegate = self
-        
-        /// Set the workout builder's data source.
-        builder.dataSource = HKLiveWorkoutDataSource(healthStore: healthStore,
-                                                     workoutConfiguration: configuration)
-    }
-    
+    /*--------------------------------------------------------------------
+     //MARK: startWorkout()
+     - Description: Start the workout.
+     -------------------------------------------------------------------*/
     func startWorkout() {
-        // Initialize our workout
         initWorkout()
-        let o2Unit = HKUnit(from: "%/min")
-        let oxySatType = HKQuantityType.quantityType(forIdentifier: .oxygenSaturation)!
-        let oxySatQuantity = HKQuantity(unit: o2Unit, doubleValue: 1)
-
-        // Start the workout session and begin data collection
         session.startActivity(with: Date())
         builder.beginCollection(withStart: Date()) { (succ, error) in
             if !succ {
                 fatalError("Error beginning collection from builder: \(String(describing: error)))")
             }
-//            let oxType = HKDiscreteQuantitySample.init(type: HKQuantityType.quantityType(forIdentifier: .oxygenSaturation)!, quantity: o2Unit, start: Date(), end: Date())
-//            self.builder.add([oxType]) { (Bool, _: Error?) in
-                
-//            }
-            //let oxySat = HKQuantitySample(type: oxySatType, quantity: oxySatQuantity, start: Date(), end: end)
         }
     }
     
+    /*--------------------------------------------------------------------
+     //MARK: stopWorkout()
+     - Description: Stop the workout.
+     -------------------------------------------------------------------*/
     func stopWorkout() {
-        // Stop the workout session
         session.end()
         builder.endCollection(withEnd: Date()) { (success, error) in
             self.builder.finishWorkout { (workout, error) in
@@ -162,68 +105,126 @@ class InterfaceController: WKInterfaceController, HKWorkoutSessionDelegate, HKLi
         }
     }
     
+    /*--------------------------------------------------------------------
+     //MARK: initWorkout()
+     - Description: Initializes the Workout Configuration.
+     -------------------------------------------------------------------*/
+    func initWorkout() {
+        let configuration = HKWorkoutConfiguration()
+        configuration.activityType = .other
+        configuration.locationType = .indoor
+        
+        do {
+            session = try HKWorkoutSession(healthStore: healthStore, configuration: configuration)
+            builder = session.associatedWorkoutBuilder()
+        } catch {
+            fatalError("Unable to create the workout session!")
+        }
+        
+        // Set the workout builder's data source.
+        builder.dataSource = HKLiveWorkoutDataSource(healthStore: healthStore, workoutConfiguration: configuration)
+        
+        // Setup session and builder.
+        session.delegate = self
+        builder.delegate = self
+    }
+    
+    /*--------------------------------------------------------------------
+     //MARK: workoutBuilder()
+     - Description: Collects sample types and sends messages.
+     -------------------------------------------------------------------*/
     func workoutBuilder(_ workoutBuilder: HKLiveWorkoutBuilder, didCollectDataOf collectedTypes: Set<HKSampleType>) {
         
         for type in collectedTypes {
             guard let quantityType = type as? HKQuantityType else {
                 return
             }
+            
             switch quantityType {
-            case HKQuantityType.quantityType(forIdentifier: .heartRate):
-                let statistics = workoutBuilder.statistics(for: quantityType)
-                let heartRateUnit = HKUnit.count().unitDivided(by: HKUnit.minute())
-                let value = statistics!.mostRecentQuantity()?.doubleValue(for: heartRateUnit)
-                sendHRMessage(hrVal: value ?? 0.00)
-                let stringValue = String(Int(Double(round(1 * value!) / 1)))
-                bpmLabel.setText(stringValue)
-                print("[workoutBuilder] Heart Rate: \(stringValue)")
-                
-            case HKQuantityType.quantityType(forIdentifier: .oxygenSaturation):
-                let statistics = workoutBuilder.statistics(for: quantityType)
-                let oSatUnit = HKUnit.count().unitDivided(by: HKUnit.minute())
-                let value = statistics!.mostRecentQuantity()?.doubleValue(for: oSatUnit)
-                let stringValue = String(Int(Double(round(1 * value!) / 1)))
-                //bpmLabel.setText(stringValue)
-                print("[workoutBuilder] Oxygen Saturation: \(stringValue)")
-            default:
-                return
+                case HKQuantityType.quantityType(forIdentifier: .heartRate):
+                    let statistics = workoutBuilder.statistics(for: quantityType)
+                    let heartRateUnit = HKUnit.count().unitDivided(by: HKUnit.minute())
+                    let value = statistics!.mostRecentQuantity()?.doubleValue(for: heartRateUnit)
+                    sendHRMessage(hrVal: value ?? 0.00)
+                    let stringValue = String(Int(Double(round(1 * value!) / 1)))
+                    bpmLabel.setText("HR: \(stringValue) BPM")
+                    print("[workoutBuilder] Heart Rate: \(stringValue)")
+                    break
+
+                case HKQuantityType.quantityType(forIdentifier: .oxygenSaturation):
+                    let statistics = workoutBuilder.statistics(for: quantityType)
+                    let oSatUnit = HKUnit.count().unitDivided(by: HKUnit.minute())
+                    let value = statistics!.mostRecentQuantity()?.doubleValue(for: oSatUnit)
+                    sendBloodOxMessage(bloodOx: value ?? 0.00)
+                    let stringValue = String(Int(Double(round(1 * value!) / 1)))
+                    bldOxLabel.setText("Blood Ox: \(stringValue) %")
+                    print("[workoutBuilder] Blood Oxygen: \(stringValue)")
+                    break
+                    
+                default:
+                    return
             }
         }
     }
     
+    /*--------------------------------------------------------------------
+     //MARK: sendHRMessage()
+     - Description: Sends message for Heart Rate
+     -------------------------------------------------------------------*/
     func sendHRMessage(hrVal: Double) {
         if (WCSession.default.isReachable) {
             let message = String(hrVal)
             WCSession.default.sendMessage(["heartRate": message], replyHandler: nil)
-            print("message sent \(message)")
+            print("HR message sent \(message)")
         } else {
-            print("message failed")
+            print("HR message failed")
         }
     }
     
+    /*--------------------------------------------------------------------
+     //MARK: sendBloodOxMessage()
+     - Description: Sends message for Blood Oxygen.
+     -------------------------------------------------------------------*/
+    func sendBloodOxMessage(bloodOx: Double) {
+        if (WCSession.default.isReachable) {
+            let message = String(bloodOx)
+            WCSession.default.sendMessage(["bloodOxygen": message], replyHandler: nil)
+            print("BldOx message sent \(message)")
+        } else {
+            print("BldOx message failed")
+        }
+    }
+    
+    /*--------------------------------------------------------------------
+     //MARK: workoutBuilderDidCollectEvent()
+     - Description: Retreive the workout event.
+     -------------------------------------------------------------------*/
     func workoutBuilderDidCollectEvent(_ workoutBuilder: HKLiveWorkoutBuilder) {
-        // Retreive the workout event.
         guard let workoutEventType = workoutBuilder.workoutEvents.last?.type else { return }
         print("[workoutBuilderDidCollectEvent] Workout Builder changed event: \(workoutEventType.rawValue)")
     }
     
+    /*--------------------------------------------------------------------
+     //MARK: workoutSession()
+     - Description: Note when state is changed.
+     -------------------------------------------------------------------*/
     func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
         print("[workoutSession] Changed State: \(toState.rawValue)")
     }
     
+    /*--------------------------------------------------------------------
+     //MARK: workoutSession()
+     - Description: Note if there was an error.
+     -------------------------------------------------------------------*/
     func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
         print("[workoutSession] Encountered an error: \(error)")
     }
     
+    /*--------------------------------------------------------------------
+     //MARK: session()
+     - Description: Note session error.
+     -------------------------------------------------------------------*/
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-
+        print("Session error: \(String(describing: error))")
     }
-
-//    func sessionDidBecomeInactive(_ session: WCSession) {
-//
-//    }
-//
-//    func sessionDidDeactivate(_ session: WCSession) {
-//
-//    }
 }
