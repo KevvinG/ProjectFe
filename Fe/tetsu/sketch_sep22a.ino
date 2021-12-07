2298053a243dfbb01811d1d3bdb84309546e30bf
grzelake <grzelake@sheridancollege.ca>
2021-12-07 19:49:44 +0000

    Correcting logic error with Arduino Serial Library

--- Fe/tetsu/sketch_sep22a.ino
+++ Fe/tetsu/sketch_sep22a.ino
@@ -110,7 +110,10 @@ void loop()
     //After gathering 25 new samples recalculate HR and SP02 and push to iOS Device
     if (validHeartRate == 1 && validSPO2 == 1) {
       String outputStr = heartRate + "," + spo2;
-      mySerial.write(outputStr)
+      int str_len = outputStr.length() + 1;
+      char s[str_len];
+      outputStr.toCharArray(s, str_len);
+      mySerial.write(s);
     }
     maxim_heart_rate_and_oxygen_saturation(irBuffer, 50, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
   }
